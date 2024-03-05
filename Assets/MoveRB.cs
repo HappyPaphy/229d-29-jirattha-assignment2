9cc5ddc7a440c27f9f82c95a35d1f338c6239ee2
HappyPaphy <jiratthakiat@gmail.com>
2024-03-05 13:40:43 +0700

    Update MoveRB Script

--- Assets/MoveRB.cs
+++ Assets/MoveRB.cs
@@ -7,18 +7,33 @@ public class MoveRB : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float moveSpeed = 15f;
     [SerializeField] private float rotateSpeed = 90f;
+    [SerializeField] private float enginePowerThrust, liftBooster, drag, angularDrag;
 
     void Start()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(rb.transform.forward * enginePowerThrust * Time.deltaTime);
+        }
+
+        Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        rb.drag = rb.velocity.magnitude * drag; 
+        rb.angularDrag = rb.velocity.magnitude * angularDrag;
+
+        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1);
+        rb.AddTorque(Input.GetAxis("Vertical") * transform.right);
+
+        /*if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(rb.transform.forward * moveSpeed * Time.deltaTime);
-        }
+        }*/
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
